13ce5f689aa7ff8e5d71f32c896a0153a9ec3276
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-09-26 16:47:42 +0530

    Create Sb201C42Application.java

---
+++ q2/src/main/java/com.masai.]/Sb201C42Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb201C42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb201C42Application.class, args);
+	}
+
+}
